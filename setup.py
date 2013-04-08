df90f441d31ab62a5d922902b28826a846653a30
memeruiz <fruiz@eie.ucr.ac.cr>
2013-04-07 20:24:11 -0600

    setup.py and Makefile added.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='avispy',
+      version='0.1',
+      description='A robot visualization tool for python',
+      author='Federico Ruiz Ugalde',
+      author_email='memeruiz@gmail.com',
+      url='http://www.arcoslab.org/',
+      packages=['avispy']
+     )
