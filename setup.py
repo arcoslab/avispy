d930a73fae28beaa29ccebafcc21e5d10b2d3780
daniel garcia vaglio <degv364@gmail.com>
2018-02-16 00:02:17 -0600

    Make setup PEP8 compliant

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 
-setup(name='avispy',
-      version='0.1',
-      description='A robot visualization tool for python',
-      author='Federico Ruiz Ugalde',
-      author_email='memeruiz@gmail.com',
-      url='http://www.arcoslab.org/',
-      packages=['avispy']
-     )
+setup(
+    name='avispy',
+    version='0.1',
+    description='A robot visualization tool for python',
+    author='Federico Ruiz Ugalde',
+    author_email='memeruiz@gmail.com',
+    url='http://www.arcoslab.org/',
+    packages=['avispy']
+)
